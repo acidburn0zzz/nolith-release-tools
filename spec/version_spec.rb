18a5dd4a35bb9f66137d6676d2c5487771bc8db8
Robert Speicher <rspeicher@gmail.com>
2015-09-01 16:04:39 -0400

    spec/vesrion_spec.rb -> spec/version_spec.rb

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 require_relative '../lib/version'
 
 describe Version do
-  describe :branch_name do
+  describe '.branch_name' do
     it { Version.branch_name('1.2.3').should == '1-2-stable' }
     it { Version.branch_name('1.23.45').should == '1-23-stable' }
     it { Version.branch_name('1.23.45.rc67').should == '1-23-stable' }
   end
 
-  describe :valid? do
+  describe '.valid?' do
     it { Version.valid?('1.2.3').should be_truthy }
     it { Version.valid?('11.22.33').should be_truthy }
     it { Version.valid?('1.2.3.rc1').should be_truthy }
