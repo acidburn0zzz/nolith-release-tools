41ab7108a3ed17b015d90a6770bfd18becce0e67
Robert Speicher <rspeicher@gmail.com>
2015-12-07 21:49:57 -0500

    Update specs for Version

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -14,7 +14,26 @@ describe Version do
     it { expect(Version.branch_name('1.2.3')).to eq '1-2-stable' }
     it { expect(Version.branch_name('1.23.45')).to eq '1-23-stable' }
     it { expect(Version.branch_name('1.23.45.rc67')).to eq '1-23-stable' }
+    it { expect(Version.branch_name('1.23.45-ee')).to eq '1-23-stable-ee' }
   end
 
+  describe '.release?' do
+    it 'is true for release versions' do
+      expect(described_class.release?('1.2.3')).to be_truthy
+    end
+
+    it 'is false for pre-release versions' do
+      expect(described_class.release?('1.2.3.rc1')).to be_falsey
+    end
+  end
+
+  describe '.rc?' do
+    it 'is true for pre-release versions' do
+      expect(described_class.rc?('1.2.3.rc1')).to be_truthy
+    end
+
+    it 'is false for release versions' do
+      expect(described_class.rc?('1.2.3')).to be_falsey
+    end
   end
 end
