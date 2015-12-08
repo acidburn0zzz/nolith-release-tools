0863d425f758fbb19d90af2d6d05075a07372e0e
Robert Speicher <rspeicher@gmail.com>
2015-12-07 23:02:50 -0500

    Update Version class
    Now it has instance methods, and the class methods delegate to those.

--- lib/version.rb
+++ lib/version.rb
@@ -1,29 +1,53 @@
-class Version
-  class << self
-    def valid?(version)
-      release?(version) || rc?(version)
-    end
+class Version < String
+  def self.branch_name(version)
+    new(version).branch_name
+  end
 
-    def branch_name(version)
-      minor_version = version.match(/\A\d+\.\d+/).to_s
+  def self.rc?(version)
+    new(version).rc?
+  end
 
-      if version.end_with?('-ee')
-        minor_version.gsub('.', '-') + '-stable-ee'
-      else
-        minor_version.gsub('.', '-') + '-stable'
-      end
-    end
+  def self.release?(version)
+    new(version).release?
+  end
 
-    def release?(version)
-      version =~ /\A\d+\.\d+\.\d+\Z/
-    end
+  def self.tag(version)
+    new(version).tag
+  end
 
-    def rc?(version)
-      version =~ /\A\d+\.\d+\.\d+\.rc\d+/
-    end
+  def self.to_minor(version)
+    new(version).to_minor
+  end
 
-    def tag(version)
-      'v' + version
+  def self.valid?(version)
+    new(version).valid?
+  end
+
+  def branch_name(force_ee: false)
+    if force_ee || self.end_with?('-ee')
+      to_minor.gsub('.', '-') + '-stable-ee'
+    else
+      to_minor.gsub('.', '-') + '-stable'
     end
+  end
+
+  def rc?
+    self =~ /\A\d+\.\d+\.\d+\.rc\d+/
+  end
+
+  def release?
+    self =~ /\A\d+\.\d+\.\d+\Z/
+  end
+
+  def tag
+    "v#{self}"
+  end
+
+  def to_minor
+    self.match(/\A\d+\.\d+/).to_s
+  end
+
+  def valid?
+    release? || rc?
   end
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,19 +2,21 @@ require 'spec_helper'
 require_relative '../lib/version'
 
 describe Version do
-  describe '.valid?' do
-    it { expect(Version.valid?('1.2.3')).to be_truthy }
-    it { expect(Version.valid?('11.22.33')).to be_truthy }
-    it { expect(Version.valid?('2.2.3.rc1')).to be_truthy }
-    it { expect(Version.valid?('1.2.3.4')).to be_falsey }
-    it { expect(Version.valid?('wow')).to be_falsey }
+  describe '.branch_name' do
+    it { expect(described_class.branch_name('1.2.3')).to eq '1-2-stable' }
+    it { expect(described_class.branch_name('1.23.45')).to eq '1-23-stable' }
+    it { expect(described_class.branch_name('1.23.45.rc67')).to eq '1-23-stable' }
+    it { expect(described_class.branch_name('1.23.45-ee')).to eq '1-23-stable-ee' }
   end
 
-  describe '.branch_name' do
-    it { expect(Version.branch_name('1.2.3')).to eq '1-2-stable' }
-    it { expect(Version.branch_name('1.23.45')).to eq '1-23-stable' }
-    it { expect(Version.branch_name('1.23.45.rc67')).to eq '1-23-stable' }
-    it { expect(Version.branch_name('1.23.45-ee')).to eq '1-23-stable-ee' }
+  describe '.rc?' do
+    it 'is true for pre-release versions' do
+      expect(described_class.rc?('1.2.3.rc1')).to be_truthy
+    end
+
+    it 'is false for release versions' do
+      expect(described_class.rc?('1.2.3')).to be_falsey
+    end
   end
 
   describe '.release?' do
@@ -27,13 +29,24 @@ describe Version do
     end
   end
 
-  describe '.rc?' do
-    it 'is true for pre-release versions' do
-      expect(described_class.rc?('1.2.3.rc1')).to be_truthy
+  describe '.tag' do
+    it 'returns a tag name' do
+      expect(described_class.tag('1.2.3.rc1')).to eq 'v1.2.3.rc1'
+      expect(described_class.tag('1.2.3')).to eq 'v1.2.3'
     end
+  end
 
-    it 'is false for release versions' do
-      expect(described_class.rc?('1.2.3')).to be_falsey
+  describe '.to_minor' do
+    it 'returns the minor version' do
+      expect(described_class.to_minor('1.23.4')).to eq '1.23'
     end
+  end
+
+  describe '.valid?' do
+    it { expect(described_class.valid?('1.2.3')).to be_truthy }
+    it { expect(described_class.valid?('11.22.33')).to be_truthy }
+    it { expect(described_class.valid?('2.2.3.rc1')).to be_truthy }
+    it { expect(described_class.valid?('1.2.3.4')).to be_falsey }
+    it { expect(described_class.valid?('wow')).to be_falsey }
   end
 end
