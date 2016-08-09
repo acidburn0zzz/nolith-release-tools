8dfb7c5bc24c252f6d490fec40e1c86c91a5c08b
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2016-08-09 10:45:52 +0200

    Add Simplecov to Gemfile and use in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # This ensure we don't push to the repo during tests
 ENV['TEST'] = 'true'
 
+require 'simplecov'
+SimpleCov.start
+
 Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
