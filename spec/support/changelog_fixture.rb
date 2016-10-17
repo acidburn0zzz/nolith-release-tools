31ecedfc8cd17dd0c0ece16d8f7e74c221c12b25
Robert Speicher <rspeicher@gmail.com>
2016-10-17 14:24:01 +0200

    Add basic EE support to ChangelogFixture

--- spec/support/changelog_fixture.rb
+++ spec/support/changelog_fixture.rb
@@ -1,7 +1,9 @@
+require 'active_support/core_ext/string'
 require 'fileutils'
 require 'rugged'
 
 require 'changelog/config'
+require 'version'
 
 # Builds a fixture repository used in testing Changelog auto-generation
 # functionality
@@ -26,13 +28,18 @@ class ChangelogFixture
     @repository = Rugged::Repository.init_at(fixture_path)
 
     build_master
-    stable_branch = build_stable_branch
+    stable_branch = build_stable_branch(Version.new('8.10.0'))
 
-    build_feature_branch
-    bugfix_branch = build_bugfix_branch
+    _feature_merge = merge_branch_with_changelog_entry(
+      changelog_path: config.ce_path,
+      changelog_name: 'group-specific-lfs'
+    )
+    bugfix_merge = merge_branch_with_changelog_entry(
+      changelog_path: config.ce_path,
+      changelog_name: 'fix-cycle-analytics-commits'
+    )
 
-    merge_commit = merge_bugfix_branch(bugfix_branch)
-    cherry_pick_to_branch(stable_branch, sha: merge_commit)
+    cherry_pick_to_branch(stable_branch, sha: bugfix_merge)
 
     repository.checkout('master')
   end
@@ -49,17 +56,24 @@ class ChangelogFixture
 
   # Set up initial `master` state
   def build_master
-    # Add a CHANGELOG.md file with example version headers and entries
+    # Add CE and EE changelogs
+    # NOTE: The contents of the EE changelog don't matter, so we can reuse CE's
     commit_blob(
       path: config.ce_log,
       content: read_fixture(config.ce_log),
       message: "Add basic #{config.ce_log}"
     )
+    commit_blob(
+      path: config.ee_log,
+      content: read_fixture(config.ce_log),
+      message: "Add basic #{config.ee_log}"
+    )
 
     # Add a VERSION file containing `8.10.0-pre`
+    # NOTE: This can safely be reused for CE and EE
     commit_blob(
       path: 'VERSION',
-      content: '8.10.0-pre',
+      content: "8.10.0-pre\n",
       message: "Update VERSION to 8.10.0-pre"
     )
 
@@ -69,88 +83,69 @@ class ChangelogFixture
       content: '',
       message: "Add #{File.join(config.ce_path, '.gitkeep')}"
     )
-  end
-
-  # Create a feature branch and merge it to `master`, then delete the branch
-  #
-  # Returns the resulting Rugged::Branch object
-  def build_feature_branch(feature_name: 'group-specific-lfs')
-    repository.checkout('master')
-
-    # Create a feature branch off of master
-    feature_branch = repository.branches.create(feature_name, 'HEAD')
-    repository.checkout(feature_branch.name)
-
-    # Commit the changelog entry
     commit_blob(
-      path: File.join(config.ce_path, "#{feature_name}#{config.extension}"),
-      content: read_fixture("#{feature_name}#{config.extension}"),
-      message: "Added group-specific settings for LFS."
+      path: File.join(config.ee_path, '.gitkeep'),
+      content: '',
+      message: "Add #{File.join(config.ee_path, '.gitkeep')}"
     )
-
-    # Merge feature branch into master
-    repository.checkout('master')
-    merge(
-      feature_branch.name,
-      'master',
-      message: "Merge branch '#{feature_branch.name}' into 'master'\n\nSee merge request !6164"
-    )
-
-    # Delete the merged branch
-    repository.branches.delete(feature_branch.name)
-
-    feature_branch
   end
 
-  # "Release" 8.10 by branching `8-10-stable` off of `master`
+  # "Release" a version by creating its stable branch off of `master`
   #
   # Returns the Rugged::Branch object for the stable branch
-  def build_stable_branch
-    stable_branch = repository.branches.create('8-10-stable', 'HEAD')
-    repository.checkout(stable_branch.name)
+  def build_stable_branch(version)
+    branch = repository.branches.create(version.stable_branch, 'HEAD')
+
+    repository.checkout(branch.name)
 
     # Update VERSION and commit
     commit_blob(
       path: 'VERSION',
-      content: '8.10.0',
-      message: 'Update VERSION to 8.10.0'
+      content: "#{version}\n",
+      message: "Update VERSION to #{version}"
     )
 
-    stable_branch
+    branch
   end
 
-  # Create a bug fix branch and merge it to `master`
+  # Create a branch and merge it to `master`, then delete the branch
+  #
+  # changelog_path - The configured changelog path
+  # changelog_name - The changelog filename (without extension)
+  #
+  # The `changelog_name` will be used to read a fixture of the same name (plus
+  # extension) and commit a blob with its contents before merging into `master`.
   #
   # Returns the resulting Rugged::Branch object
-  def build_bugfix_branch(bugfix_name: 'fix-cycle-analytics-commits')
+  def merge_branch_with_changelog_entry(changelog_path:, changelog_name:)
     repository.checkout('master')
 
-    # Create a bugfix branch off of master
-    bugfix_branch = repository.branches.create(bugfix_name, 'HEAD')
-    repository.checkout(bugfix_branch.name)
+    # Create a feature branch off of master
+    branch = repository.branches.create(changelog_name, 'HEAD')
+    repository.checkout(branch.name)
+
+    fixture = read_fixture("#{changelog_name}#{config.extension}")
+    entry   = load_fixture(fixture)
 
     # Commit the changelog entry
     commit_blob(
-      path: File.join(config.ce_path, "#{bugfix_name}#{config.extension}"),
-      content: read_fixture("#{bugfix_name}#{config.extension}"),
-      message: 'Fix an issue with the "Commits" section of the cycle analytics summary.'
+      path: File.join(changelog_path, "#{changelog_name}#{config.extension}"),
+      content: fixture,
+      message: entry['title']
     )
 
-    bugfix_branch
-  end
+    # Merge branch into master
+    merge_commit = merge(branch.name, 'master', message: <<-MSG.strip_heredoc)
+      Merge branch '#{branch.name}' into 'master'
 
-  # Merge `source_branch` into `master`
-  #
-  # Returns a merge commit SHA
-  def merge_bugfix_branch(source_branch)
-    # Merge into master
-    repository.checkout('master')
-    merge_commit = merge(source_branch.name, 'master', {
-      message: "Merge branch '#{source_branch.name}' into 'master'\n\nSee merge request !6164"
-    })
+      #{entry['title']}
+
+      See merge request !#{entry['id']}
+    MSG
 
     # Delete the merged branch
-    repository.branches.delete(source_branch.name)
+    repository.checkout('master')
+    repository.branches.delete(branch.name)
 
     merge_commit
   end
@@ -160,19 +155,16 @@ class ChangelogFixture
     repository.checkout(branch.name)
 
     # cherry-pick the merge commit
-    merge_commit = repository.lookup(sha)
-    pick_index = repository.cherrypick_commit(
-      merge_commit,
-      repository.head.target,
-      1
-    )
+    commit = repository.lookup(sha)
+    target = repository.head.target
+    pick_index = repository.cherrypick_commit(commit, target, 1)
 
     # Commit the pick
     Rugged::Commit.create(
       repository,
       tree: pick_index.write_tree(repository),
-      message: merge_commit.message,
-      parents: [repository.head.target],
+      message: commit.message,
+      parents: [target],
       update_ref: 'HEAD'
     )
 
@@ -223,5 +215,9 @@ class ChangelogFixture
 
   def read_fixture(filename)
     File.read(File.expand_path("../fixtures/changelog/#{filename}", __dir__))
+  end
+
+  def load_fixture(contents)
+    YAML.load(contents)
   end
 end
