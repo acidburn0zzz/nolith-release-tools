f83cd5f1ad81bd1864dd419f257586f2d987167e
Rémy Coutable <remy@rymai.me>
2016-11-17 12:28:01 +0100

    Don't redefine `#before_execute_hook` in GitlabEeRelease
    We already inherit this method from GitlabCeRelease!
    
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- lib/release/gitlab_ee_release.rb
+++ lib/release/gitlab_ee_release.rb
@@ -4,12 +4,6 @@ module Release
   class GitlabEeRelease < GitlabCeRelease
     private
 
-    def before_execute_hook
-      compile_changelog
-
-      super
-    end
-
     def remotes
       Remotes.remotes(:ee, dev_only: options[:security])
     end
