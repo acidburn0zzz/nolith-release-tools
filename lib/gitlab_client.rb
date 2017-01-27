5e9b7a84a5a28d4d09f0eb7bc29f50767802aa76
Gabriel Mazetto <brodock@gmail.com>
2017-01-27 18:55:29 +0100

    Refactor GitlabClient and added GitlabDevClient
    Both use different ENV credentials

--- lib/gitlab_client.rb
+++ lib/gitlab_client.rb
@@ -7,63 +7,71 @@ class GitlabClient
     end
   end
 
-  # Hard-code the CE project ID to save a request
-  CE_PROJECT_ID = 13_083
+  # Hard-code IDs following the 'namespace%2Frepo' pattern
+  CE_PROJECT_ID = 'gitlab-org%2Fgitlab-ce'.freeze
 
-  def self.current_user
-    @current_user ||= Gitlab.user
-  end
+  class << self
+    def current_user
+      @current_user ||= Gitlab.user
+    end
 
-  def self.ce_issues(options = {})
-    Gitlab.issues(CE_PROJECT_ID, options)
-  end
+    def ce_issues(options = {})
+      client.issues(CE_PROJECT_ID, options)
+    end
 
-  def self.ce_milestones(options = {})
-    Gitlab.milestones(CE_PROJECT_ID, options)
-  end
+    def ce_milestones(options = {})
+      client.milestones(CE_PROJECT_ID, options)
+    end
 
-  def self.ce_milestone(title)
-    ce_milestones
+    def ce_milestone(title)
+      ce_milestones
       .detect { |m| m.title == title } || MissingMilestone.new
-  end
+    end
 
-  # Create an issue in the CE project based on the provided issue
-  #
-  # issue - An object that responds to the following messages:
-  #         :title       - Issue title String
-  #         :description - Issue description String
-  #         :labels      - Comma-separated String of label names
-  #         :version     - Version object
-  #
-  # The issue is always assigned to the authenticated user.
-  #
-  # Returns a Gitlab::ObjectifiedHash object
-  def self.create_issue(issue)
-    milestone = ce_milestone(issue.version.milestone_name)
+    # Create an issue in the CE project based on the provided issue
+    #
+    # issue - An object that responds to the following messages:
+    #         :title       - Issue title String
+    #         :description - Issue description String
+    #         :labels      - Comma-separated String of label names
+    #         :version     - Version object
+    #
+    # The issue is always assigned to the authenticated user.
+    #
+    # Returns a Gitlab::ObjectifiedHash object
+    def create_issue(issue)
+      milestone = ce_milestone(issue.version.milestone_name)
 
-    Gitlab.create_issue(CE_PROJECT_ID, issue.title, {
-      description:  issue.description,
-      assignee_id:  current_user.id,
-      milestone_id: milestone.id,
-      labels:       issue.labels,
-      confidential: issue.confidential?
-    })
-  end
+      client.create_issue(CE_PROJECT_ID, issue.title, {
+        description:  issue.description,
+        assignee_id:  current_user.id,
+        milestone_id: milestone.id,
+        labels:       issue.labels,
+        confidential: issue.confidential?
+      })
+    end
 
-  # Find an issue in the CE project based on the provided issue
-  #
-  # issue - An object that responds to the following messages:
-  #         :title  - Issue title String
-  #         :labels - Comma-separated String of label names
-  #
-  # Returns a Gitlab::ObjectifiedHash object, or nil
-  def self.find_issue(issue)
-    opts = { labels: issue.labels, milestone: issue.version.milestone_name }
+    # Find an issue in the CE project based on the provided issue
+    #
+    # issue - An object that responds to the following messages:
+    #         :title  - Issue title String
+    #         :labels - Comma-separated String of label names
+    #
+    # Returns a Gitlab::ObjectifiedHash object, or nil
+    def find_issue(issue)
+      opts = { labels: issue.labels, milestone: issue.version.milestone_name }
 
-    ce_issues(opts).detect { |i| i.title == issue.title }
-  end
+      ce_issues(opts).detect { |i| i.title == issue.title }
+    end
 
-  def self.issue_url(issue)
-    "https://gitlab.com/gitlab-org/gitlab-ce/issues/#{issue.iid}"
+    def issue_url(issue)
+      "https://gitlab.com/gitlab-org/gitlab-ce/issues/#{issue.iid}"
+    end
+
+    private
+
+    def client
+      @client ||= Gitlab.client(endpoint: ENV['GITLAB_API_ENDPOINT'], private_token: ENV['GITLAB_API_PRIVATE_TOKEN'])
+    end
   end
 end
---
+++ lib/gitlab_dev_client.rb
@@ -0,0 +1,31 @@
+require 'gitlab'
+
+class GitlabDevClient
+  # Hard-code IDs following the 'namespace%2Frepo' pattern
+  CE_PROJECT_DEV_ID = 'gitlab%2Fgitlabhq'.freeze
+  EE_PROJECT_DEV_ID = 'gitlab%2Fgitlab-ee'.freeze
+
+  class << self
+    def ce_create_variable(value)
+      client.create_variables(CE_PROJECT_DEV_ID, 'PACKAGECLOUD_REPO', value)
+    end
+
+    def ce_remove_variable(value)
+      client.remove_variables(CE_PROJECT_DEV_ID, value)
+    end
+
+    def ee_create_variable(value)
+      client.create_variables(EE_PROJECT_DEV_ID, 'PACKAGECLOUD_REPO', value)
+    end
+
+    def ee_remove_variable(value)
+      client.remove_variable(EE_PROJECT_DEV_ID, value)
+    end
+
+    private
+
+    def client
+      @client ||= Gitlab.client(endpoint: ENV['GITLAB_DEV_API_ENDPOINT'], private_token: ENV['GITLAB_DEV_API_PRIVATE_TOKEN'])
+    end
+  end
+end
