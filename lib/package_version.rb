6caf7bde00414e7436f78785f83592d0ebbaedf4
Gabriel Mazetto <brodock@gmail.com>
2017-01-31 01:59:34 +0100

    typecast PackageVersion matches and added some helper methods

--- lib/package_version.rb
+++ lib/package_version.rb
@@ -2,22 +2,30 @@ class PackageVersion < String
   REGEXP = /\Agitlab-(?<edition>ce|ee)[-_](?<major>\d+)\.(?<minor>\d+)\.(?<patch>\d+)-(ce|ee)\.(?<revision>\d+)(_(?<arch>amd64|armhf)|\.(?<distro>el\d+|sles\d+)\.(?<arch>x86_64))\.(?<pkgtype>deb|rpm)\z/
 
   def edition
-    REGEXP.match(self)[:edition]
+    REGEXP.match(self)[:edition].to_sym
   end
 
   def major
-    REGEXP.match(self)[:major]
+    REGEXP.match(self)[:major].to_i
   end
 
   def minor
-    REGEXP.match(self)[:minor]
+    REGEXP.match(self)[:minor].to_i
   end
 
   def patch
-    REGEXP.match(self)[:patch]
+    REGEXP.match(self)[:patch].to_i
   end
 
   def revision
-    REGEXP.match(self)[:revision]
+    REGEXP.match(self)[:revision].to_i
+  end
+
+  def ee?
+    edition == :ee
+  end
+
+  def ce?
+    edition == :ce
   end
 end
