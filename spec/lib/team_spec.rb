15d4da73fc265f2b95fa8512458963b9ca899ec2
Rémy Coutable <remy@rymai.me>
2017-11-06 18:52:48 +0100

    Allow Team#initialize to take an array of members
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/lib/team_spec.rb
+++ spec/lib/team_spec.rb
@@ -2,38 +2,42 @@ require 'spec_helper'
 
 require 'team'
 
-describe Team, vcr: { cassette_name: 'team' } do
-  describe '#to_a' do
-    it 'returns an array of TeamMember' do
-      expect(subject.to_a.all? { |member| member.is_a?(TeamMember) }).to be(true)
+describe Team do
+  let(:team_member_1) { TeamMember.new(name: 'Mickael Mike', username: 'mike') }
+
+  describe '#initialize' do
+    it 'default to the GitLab Inc team' do
+      expect(HTTParty).to receive(:get)
+        .with('https://gitlab.com/gitlab-com/www-gitlab-com/raw/master/data/team.yml')
+        .and_return(double(body: YAML.dump([{ 'name' => 'Sid Sijbrandij', 'gitlab' => 'sytses' }])))
+
+      expect(subject.find_by_name('Sid Sijbrandij').username).to eq('sytses')
     end
 
-    [
-      'lbennett',
-      'axil',
-      'ayufan',
-      'rdavila',
-      'nick.thomas',
-      'godfat',
-      'dbalexandre',
-      'jarka',
-      'balasankarc',
-      'winh',
-      'kushalpandya'
-    ].each do |username|
-      it "includes team member: #{username}" do
-        expect(subject.to_a.find { |member| member.username == username }).to be_truthy
-      end
+    it 'accepts a custom team' do
+      team = described_class.new(members: [team_member_1])
+
+      expect(team.to_a).to eq([team_member_1])
+    end
+  end
+
+  describe '#to_a' do
+    subject { described_class.new(members: [team_member_1]) }
+
+    it 'returns an array of TeamMember' do
+      expect(subject.to_a).to eq([team_member_1])
     end
   end
 
   describe '#find_by_name' do
+    subject { described_class.new(members: [team_member_1]) }
+
     context 'when the team member exists' do
       it 'returns a TeamMember' do
-        member = subject.find_by_name('Rémy Coutable')
+        member = subject.find_by_name('Mickael Mike')
 
-        expect(member.name).to eq('Rémy Coutable')
-        expect(member.username).to eq('rymai')
+        expect(member.name).to eq('Mickael Mike')
+        expect(member.username).to eq('mike')
       end
     end
 
