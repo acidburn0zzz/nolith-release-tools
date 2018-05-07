657cecf3765d7230cb5b0e1556520a79fe2939a3
Robert Speicher <rspeicher@gmail.com>
2018-05-07 15:25:20 -0500

    Add `DEV_API_PRIVATE_TOKEN` environment variable
    Removes `*_API_AUTH_TOKEN` filter from VCR configuration, because it was
    never used.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,7 +6,6 @@ VCR.configure do |c|
   c.default_cassette_options = { record: :new_episodes }
   c.hook_into :webmock
 
-  %w[API_AUTH_TOKEN PRIVATE_TOKEN].each do |val|
-    c.filter_sensitive_data("[GITLAB_API_#{val}]") { ENV["GITLAB_API_#{val}"] }
-  end
+  c.filter_sensitive_data('[GITLAB_API_PRIVATE_TOKEN]') { ENV['GITLAB_API_PRIVATE_TOKEN'] }
+  c.filter_sensitive_data('[DEV_API_PRIVATE_TOKEN]') { ENV['DEV_API_PRIVATE_TOKEN'] }
 end
