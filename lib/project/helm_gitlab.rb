a89554b891db99bd5a5075742977118d063b1841
DJ Mountney <dj@gitlab.com>
2018-06-29 16:52:23 -0700

    Drop https remote hack that was temporarily used for a CI user

--- lib/project/helm_gitlab.rb
+++ lib/project/helm_gitlab.rb
@@ -3,7 +3,7 @@ require_relative 'base_project'
 module Project
   class HelmGitlab < BaseProject
     REMOTES = {
-      gitlab: ENV['USE_GIT_HTTPS'] ? 'https://gitlab.com/charts/gitlab.git' : 'git@gitlab.com:charts/gitlab.git',
+      gitlab: 'git@gitlab.com:charts/gitlab.git',
     }.freeze
 
     def self.path
