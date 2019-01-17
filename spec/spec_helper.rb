d448577d5d9dc8743f40856519ddb2472f8358de
Robert Speicher <rspeicher@gmail.com>
2019-01-17 12:43:43 -0600

    Add persistence file when outside of CI
    This allows you to run `rspec --only-failures` to re-run only failing
    specs from the previous run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,10 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.order = 'random'
 
+  unless ENV['CI']
+    config.example_status_persistence_file_path = './spec/examples.txt'
+  end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
