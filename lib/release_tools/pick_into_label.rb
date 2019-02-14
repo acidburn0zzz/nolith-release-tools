a665b4f5ae201c65cc577b0fc74ec958c0dda7d2
Robert Speicher <rspeicher@gmail.com>
2019-02-14 14:20:34 -0800

    Add `PickIntoLabel.create` method
    This will use GitlabClient to create a group label under the
    `gitlab-org` namespace with the appropriate title, color, and
    description for our monthly "Pick into X.Y" labels.
    
    We do not yet handle cleaning up old labels.

--- lib/release_tools/pick_into_label.rb
+++ lib/release_tools/pick_into_label.rb
@@ -2,6 +2,19 @@
 
 module ReleaseTools
   class PickIntoLabel
+    COLOR = '#00C8CA'
+    DESCRIPTION = 'Merge requests to cherry-pick into the `%s` branch.'
+
+    # Create a group label for the specified version
+    def self.create(version)
+      GitlabClient.create_group_label(
+        Project::GitlabCe.group,
+        self.for(version),
+        COLOR,
+        description: DESCRIPTION % version.stable_branch
+      )
+    end
+
     def self.escaped(version)
       CGI.escape(self.for(version))
     end
--- spec/lib/release_tools/pick_into_label_spec.rb
+++ spec/lib/release_tools/pick_into_label_spec.rb
@@ -1,6 +1,23 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe ReleaseTools::PickIntoLabel do
+  describe '.create' do
+    it 'creates a group label for the specified version' do
+      version = ReleaseTools::Version.new('11.8.0-rc1')
+
+      expect(ReleaseTools::GitlabClient).to receive(:create_group_label).with(
+        'gitlab-org',
+        'Pick into 11.8',
+        described_class::COLOR,
+        description: described_class::DESCRIPTION % '11-8-stable'
+      )
+
+      described_class.create(version)
+    end
+  end
+
   describe '.escaped' do
     it 'returns the correct label' do
       version = instance_double('Version', to_minor: 'foo')
