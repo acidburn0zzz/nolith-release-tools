8f7755110357874635d6d24eac801916704232c6
Balasankar "Balu" C <balasankar@gitlab.com>
2019-03-12 16:12:01 +0530

    Append v for version strings
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- lib/release_tools/release/cng_image_release.rb
+++ lib/release_tools/release/cng_image_release.rb
@@ -11,18 +11,22 @@ module ReleaseTools
         target_file = File.join(repository.path, 'ci_files/variables.yml')
 
         yaml_contents = YAML.load_file(target_file)
-        yaml_contents['variables']['GITLAB_VERSION'] = version
-        yaml_contents['variables']['GITALY_VERSION'] = read_file_from_gitlab_repo('GITALY_SERVER_VERSION')
-        yaml_contents['variables']['GITLAB_SHELL_VERSION'] = read_file_from_gitlab_repo('GITLAB_SHELL_VERSION')
-        yaml_contents['variables']['GITLAB_WORKHORSE_VERSION'] = read_file_from_gitlab_repo('GITLAB_WORKHORSE_VERSION')
-        yaml_contents['variables']['GITLAB_REF_SLUG'] = version
-        yaml_contents['variables']['GITLAB_ASSETS_TAG'] = version
+        yaml_contents['variables']['GITLAB_VERSION'] = version_string(version)
+        yaml_contents['variables']['GITALY_VERSION'] = version_string(read_file_from_gitlab_repo('GITALY_SERVER_VERSION'))
+        yaml_contents['variables']['GITLAB_SHELL_VERSION'] = version_string(read_file_from_gitlab_repo('GITLAB_SHELL_VERSION'))
+        yaml_contents['variables']['GITLAB_WORKHORSE_VERSION'] = version_string(read_file_from_gitlab_repo('GITLAB_WORKHORSE_VERSION'))
+        yaml_contents['variables']['GITLAB_REF_SLUG'] = version_string(version)
+        yaml_contents['variables']['GITLAB_ASSETS_TAG'] = version_string(version)
 
         File.open(target_file, 'w') do |f|
           f.write(YAML.dump(yaml_contents))
         end
 
         repository.commit(target_file, message: "Update #{target_file} for #{version}")
+      end
+
+      def version_string(version)
+        "v#{version}"
       end
 
       def read_file_from_gitlab_repo(file_name)
