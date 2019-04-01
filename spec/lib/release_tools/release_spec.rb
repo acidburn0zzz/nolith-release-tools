f185e258840ba7591fbaf90f74d6c6f3b265da8a
Robert Speicher <rspeicher@gmail.com>
2019-04-01 12:43:32 +0200

    Correct Timecop usage to avoid cross-test pollution
    See https://gitlab.com/gitlab-org/release-tools/merge_requests/569#note_155999053

--- spec/lib/release_tools/release_spec.rb
+++ spec/lib/release_tools/release_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe ReleaseTools::Release do
   describe '.next_date' do
     it 'defaults to the 22nd of this month' do
-      Timecop.travel(Time.local(2015, 12, 1))
-
-      expect(described_class.next_date).to eq Date.new(2015, 12, 22)
+      Timecop.freeze(Time.local(2015, 12, 1)) do
+        expect(described_class.next_date).to eq Date.new(2015, 12, 22)
+      end
     end
 
     it 'uses the 22nd of next month when after the 15th' do
-      Timecop.travel(Time.local(2015, 12, 15))
-
-      expect(described_class.next_date).to eq Date.new(2016, 1, 22)
+      Timecop.freeze(Time.local(2015, 12, 15)) do
+        expect(described_class.next_date).to eq Date.new(2016, 1, 22)
+      end
     end
   end
 end
