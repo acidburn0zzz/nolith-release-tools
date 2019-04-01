1e73853d4b7706c47b195487afefccb5b11c280b
Yorick Peterse <yorickpeterse@gmail.com>
2019-04-01 15:58:43 +0200

    Update security template for the new merging setup

--- spec/lib/release_tools/security_patch_issue_spec.rb
+++ spec/lib/release_tools/security_patch_issue_spec.rb
@@ -20,19 +20,6 @@ describe ReleaseTools::SecurityPatchIssue do
   end
 
   describe '#description' do
-    it 'includes steps to push to dev only' do
-      issue = described_class.new(version: ReleaseTools::Version.new('8.3.1-rc2'))
-
-      content = issue.description
-
-      aggregate_failures do
-        expect(content).to include '**Push `ce/8-3-stable` to `dev` only: `git push dev 8-3-stable`**'
-        expect(content).to include '**Push `ee/8-3-stable-ee` to `dev` only: `git push dev 8-3-stable-ee`**'
-        expect(content).to include '**Push `omnibus-gitlab/8-3-stable` to `dev` only: `git push dev 8-3-stable`**'
-        expect(content).to include '**Push `omnibus-gitlab/8-3-stable-ee` to `dev` only: `git push dev 8-3-stable-ee`**'
-      end
-    end
-
     it 'includes a step to create the blog post in a private snippet' do
       issue = described_class.new(version: ReleaseTools::Version.new('8.3.1'))
 
