48ef3a9abcd2b1f3792424309c52f1cf552e90f3
Robert Speicher <rspeicher@gmail.com>
2019-07-26 15:42:14 -0500

    Remove process info from CI logs

--- lib/release_tools/logger.rb
+++ lib/release_tools/logger.rb
@@ -12,6 +12,13 @@ module ReleaseTools
         nil
       end
     end
+
+    # Remove process info from the Default formatter
+    class NoProcessDefaultFormatter < SemanticLogger::Formatters::Default
+      def process_info
+        nil
+      end
+    end
   end
 end
 
@@ -20,7 +27,10 @@ SemanticLogger.default_level = ENV.fetch('LOG_LEVEL', 'debug').to_sym
 
 if File.basename($PROGRAM_NAME) == 'rspec'
   # Overwrite each test run; meaningless in CI but nice for development
-  SemanticLogger.add_appender(io: File.new('log/test.log', 'w'))
+  SemanticLogger.add_appender(
+    io: File.new('log/test.log', 'w'),
+    formatter: ReleaseTools::Logger::NoProcessDefaultFormatter.new
+  )
 else
   SemanticLogger.add_appender(
     io: $stdout,
