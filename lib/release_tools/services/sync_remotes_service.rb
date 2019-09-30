f315e1b8d711dc1a3d419bb8367d3db96b9bfb8e
Robert Speicher <rspeicher@gmail.com>
2019-09-30 09:51:57 -0500

    Add project to SyncRemotesService log payloads

--- lib/release_tools/services/sync_remotes_service.rb
+++ lib/release_tools/services/sync_remotes_service.rb
@@ -35,12 +35,12 @@ module ReleaseTools
         repository = RemoteRepository.get(project.remotes, global_depth: 1)
 
         tags.each do |tag|
-          logger.info('Fetching tag', name: tag)
+          logger.info('Fetching tag', project: project, name: tag)
 
           repository.fetch("refs/tags/#{tag}", remote: :dev)
 
           if Feature.enabled?(:publish_git_push) # rubocop:disable Style/Next
-            logger.info('Pushing tag to all remotes', name: tag)
+            logger.info('Pushing tag to all remotes', project: project, name: tag)
 
             repository.push_to_all_remotes(tag)
           end
@@ -61,12 +61,12 @@ module ReleaseTools
 
           if result.status.success?
             if Feature.enabled?(:publish_git_push)
-              logger.info('Pushing branch to all remotes', name: branch)
+              logger.info('Pushing branch to all remotes', project: project, name: branch)
 
               repository.push_to_all_remotes(branch)
             end
           else
-            logger.fatal('Failed to sync branch', name: branch, output: result.output)
+            logger.fatal('Failed to sync branch', project: project, name: branch, output: result.output)
           end
         end
       end
