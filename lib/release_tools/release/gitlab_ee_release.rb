d74d4520ab803ff616e71abc399c17a9cec37810
Hossein Pursultani <hossein@pursultani.net>
2019-12-06 16:19:54 +1100

    Add UBI-based CNG images to release process

--- lib/release_tools/release/gitlab_ee_release.rb
+++ lib/release_tools/release/gitlab_ee_release.rb
@@ -8,6 +8,19 @@ module ReleaseTools
       def remotes
         Project::GitlabEe.remotes
       end
+
+      def after_execute_hook
+        super
+
+        # UBI-based CNG image release
+        begin
+          Release::CNGImageRelease
+            .new(version, options.merge(gitlab_repo_path: repository.path, ubi: true))
+            .execute
+        rescue StandardError => ex
+          logger.fatal('UBI-based CNG image release failed', error: ex.message)
+        end
+      end
     end
   end
 end
